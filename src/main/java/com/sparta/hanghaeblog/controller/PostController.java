f43baccdc7be24f75d4d1bf6e0ad15f2302f52a7
seongjun <seongjuni1@naver.com>
2022-12-15 10:39:32 +0900

    수정:controller exception message modify

--- src/main/java/com/sparta/hanghaeblog/controller/PostController.java
+++ src/main/java/com/sparta/hanghaeblog/controller/PostController.java
@@ -6,6 +6,7 @@ import com.sparta.hanghaeblog.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,45 +15,46 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/api/posts")
+@ResponseBody
 public class PostController {
     private final PostService postService;
 
-    @GetMapping("/api/posts")
-    @ResponseBody
+    @GetMapping("")
     public List<PostResponseDto> getAllPost(){
         return postService.getAllPost();
     }
 
-    @PostMapping("/api/posts")
-    @ResponseBody
+    @PostMapping("")
     public PostResponseDto createPost(@Validated @RequestBody PostRequestDto postRequestDto, HttpServletRequest request){
         return postService.createPost(postRequestDto, request);
     }
 
-    @GetMapping("/api/posts/{id}")
-    @ResponseBody
+    @GetMapping("/{id}")
     public PostResponseDto getPost(@PathVariable Long id){
         return postService.getPost(id);
     }
 
-    @PutMapping("/api/posts/{id}")
-    @ResponseBody
+    @PutMapping("/{id}")
     public PostResponseDto updatePost(
             @PathVariable Long id, @RequestBody @Validated PostRequestDto postRequestDto, HttpServletRequest request){
         return postService.updatePost(id, postRequestDto, request);
     }
 
-    @DeleteMapping("/api/posts/{id}")
-    @ResponseBody
+    @DeleteMapping("/{id}")
     public String deletePost(@PathVariable Long id,HttpServletRequest request){
         postService.deletePost(id, request);
         return "delete success";
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseBody
     public String IllegalArgumentExceptionMessage(IllegalArgumentException e) {
-        return e.toString();
+        return e.toString().substring(e.toString().indexOf(":")+2);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String MethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return "빈칸을 입력해 주세요";
     }
 
 }
--- src/main/java/com/sparta/hanghaeblog/controller/UserController.java
+++ src/main/java/com/sparta/hanghaeblog/controller/UserController.java
@@ -6,6 +6,7 @@ import com.sparta.hanghaeblog.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -13,27 +14,30 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
+@ResponseBody
 public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    @ResponseBody
     public String signup(@Validated @RequestBody SignupRequestDto signupRequestDto){
         userService.signup(signupRequestDto);
         return "signup success";
     }
 
     @PostMapping("/login")
-    @ResponseBody
     public String login(@Validated @RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response){
         userService.login(loginRequestDto, response);
         return "login success";
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseBody
     public String IllegalArgumentExceptionMessage(IllegalArgumentException e) {
-        return e.toString();
+        return e.toString().substring(e.toString().indexOf(":")+2);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String MethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return "이름은 4~10글자 알파벳(a~z), 숫자(0~9)로 구성되어야 합니다\n" +
+                "비밀번호는 8~15글자 알파벳(A~Z, a~z), 숫자(0~9)로 구성되어야 합니다";
+    }
 }
