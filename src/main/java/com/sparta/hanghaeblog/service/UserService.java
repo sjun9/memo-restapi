09a22ec0d8628f0ea5917381704643d1fcbbb15f
seongjun <seongjuni1@naver.com>
2023-03-05 23:58:34 +0900

    action test

--- src/main/java/com/sparta/hanghaeblog/service/UserService.java
+++ src/main/java/com/sparta/hanghaeblog/service/UserService.java
@@ -45,7 +45,6 @@ public class UserService {
         User user = userRepository.findByUsername(username).orElseThrow(
                 () -> new IllegalArgumentException("등록된 사용자가 없습니다")
         );
-
         if(!passwordEncoder.matches(password,user.getPassword())){
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다");
         }
