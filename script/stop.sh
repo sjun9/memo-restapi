cef66141bee1f2dce3d9518ec94c507f37ac6109
seongjun <117061026+sjun9@users.noreply.github.com>
2023-03-06 10:49:08 +0900

    Update stop.sh
    Signed-off-by: seongjun <117061026+sjun9@users.noreply.github.com>

--- script/stop.sh
+++ script/stop.sh
@@ -14,5 +14,6 @@ CURRENT_PID=$(pgrep -f $JAR_FILE)
   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
 else
   echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  chmod +x script.sh
   kill -15 $CURRENT_PID
 fi
